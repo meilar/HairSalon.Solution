eb184922e294a051c7af7887717c9fdbfa2a4dec
Matt Eilar <meilar@gmail.com>
2022-03-11 09:14:13 -0800

    add model classes

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,15 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+    public string Name { get; set; }
+
+    public Client(string name)
+    {
+      Name = name;
+    }
+  }
+}
